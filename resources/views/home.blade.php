e48adf5f57ddf51f9247739630db215492ac6821
nguyên trần <User@TPN-TNH.TPN-TNH>
2025-10-15 20:21:54 +0700

    Initial source project

---
+++ resources/views/home.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home | Movie Ticket Booking</title>
+</head>
+<body style="font-family: Arial, sans-serif; text-align: center; margin-top: 100px;">
+    <h1 style="color: darkblue;">This is HOME of MOVIE-TICKET-BOOKING</h1>
+</body>
+</html>
